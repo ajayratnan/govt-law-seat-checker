339a584d213f0b14a4b58afeeabce7f0af61c3c7
ajayratnan <65624062+ajayratnan@users.noreply.github.com>
2025-07-23 18:35:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import GovtLawCollegeSeatPredictor from "./GovtLawCollegeSeatPredictor";
+import { Analytics } from "@vercel/analytics/react";
 import "./index.css";   // Tailwind base styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GovtLawCollegeSeatPredictor />
+      <Analytics />        {/* ← add */}
   </React.StrictMode>
 );
